219f5f6c41ec220c72a5d737f707d9b40ef417a3
Marta <sliusar18@gmail.com>
2023-05-18 22:05:29 +0300

    corrected

--- task4.py
+++ task4.py
@@ -2,12 +2,14 @@ lst = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 suma = 35
 i = 0
 j = 0
-pairs = []
+pairs = set()
 
 for i in range(len(lst)):
     for j in range(i+1, len(lst)):
         if lst[i] + lst[j] == suma:
-            result = (lst[i], lst[j])
-            pairs.append(result)
-print(pairs)
+            result1 = lst[i], lst[j]
+            pairs.add(result1)
+            result2 = result1[::-1]
+            pairs.add(result2)
+print(sorted(pairs))
 
